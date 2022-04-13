c9158fed91fd9008703de17b8b46526ea8ef2755
KhStasya <htej.anastasiya.ig+30@gmail.com>
2022-04-13 22:38:53 +0200

    1st lesson commit

---
+++ Lesson1/main.py
@@ -0,0 +1,27 @@
+# 1.Создать 3 переменных с одинаковыми данными с одинаковыми идентификаторами
+
+age_one = age_two = age_three = 20
+
+print(age_one, id(age_one))
+print(age_two, id(age_two))
+print(age_three, id(age_three))
+
+# 2.Создать 2 перменных с одинаковыми данными с разными идентификаторами
+age_four = 257.0
+print(age_four, id(age_four))
+age_five = 257
+print(age_five, id(age_five))
+
+# 3.Поменять их типы так, чтобы у 1х трех были разные идентификаторы,а у 2х последних были одинаковые
+age_one = '20'
+age_two = 20.0
+age_three = 20
+print(age_one, id(age_one))
+print(age_two, id(age_two))
+print(age_three, id(age_three))
+
+
+age_four = 257
+age_five = 257
+print(age_four, id(age_four))
+print(age_five, id(age_five))
