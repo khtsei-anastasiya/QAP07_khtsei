c0ba01930311d697faa1f256cc6ebd641b4ae7d2
KhStasya <htej.anastasiya.ig+30@gmail.com>
2022-04-14 10:14:22 +0200

    1st lesson commit

--- Lesson1/main.py
+++ Lesson1/main.py
@@ -7,9 +7,9 @@ print(age_two, id(age_two))
 print(age_three, id(age_three))
 
 # 2.Создать 2 перменных с одинаковыми данными с разными идентификаторами
-age_four = 257.0
+age_four = [1, 2, 3]
 print(age_four, id(age_four))
-age_five = 257
+age_five = [1, 2, 3]
 print(age_five, id(age_five))
 
 # 3.Поменять их типы так, чтобы у 1х трех были разные идентификаторы,а у 2х последних были одинаковые
