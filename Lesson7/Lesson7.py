f3e91b946eb793713fae8322cb1435b71be68fff
KhStasya <htej.anastasiya.ig+30@gmail.com>
2022-05-10 15:10:40 +0200

    hw 7 10.05.22

--- Lesson7/Lesson7.py
+++ Lesson7/Lesson7.py
@@ -10,10 +10,12 @@
 # машины, каждого самолета,
 # вызвать различные методы у этих объектов. «Поиграться», скажем так. А затем сделать коллекцию из этих объектов и
 # через цикл пройтись по каждому из этих объектов и вызвать те методы, которые есть во всех классах.
+import self as self
+
 
 class BasicCar:
 
-    def __init__(self, model='', engine_type='', max_speed='', ):
+    def __init__(self, model='', engine_type='', max_speed=''):
         self.model = model
         self.engine_type = engine_type
         self.max_speed = max_speed
@@ -46,7 +48,7 @@ class Tesla(BasicCar):
         print('Roads driving\n')
 
     @property
-    def full_car_info(self):
+    def full_machine_info(self):
         return self.info(), self.autopilot()
 
 
@@ -67,15 +69,84 @@ class LandRover(BasicCar):
         print('Off-roads driving\n')
 
     @property
-    def full_car_info(self):
+    def full_machine_info(self):
         return self.info(), self.geartype()
+
+
+class Plane:
+
+    def __init__(self, model='', engine_type='', max_speed='', variant='', cruising_altitude=''):
+        self.model = model
+        self.engine_type = engine_type
+        self.max_speed = max_speed
+        self.variant = variant
+        self.cruising_altitude = cruising_altitude
+
+    def info(self):
+        print(f'Plane model is "{self.model}{self.variant}" \n'
+              f'Engine type is "{self.engine_type}" \n'
+              f'Max speed is "{self.max_speed}" km/h \n'
+              f'Cruising altitude is "{self.cruising_altitude}" metres')
+
+    def drive_type(self):
+        pass
+
+    def autopilot(self):
+        pass
+
+
+class Boeing(Plane):
+
+    def __init__(self, variant='', length=''):
+        super().__init__(model='Boeing 737 ', engine_type='high bypass turbofan engine', max_speed='898',
+                         cruising_altitude='')
+        self.length = length
+        self.variant = variant
+        if self.length == 94:
+            self.variant = '100'
+            self.cruising_altitude = '10,670'
+        elif self.length == 100:
+            self.variant = '200'
+            self.cruising_altitude = '10,670'
+        elif self.length in (102, 120):
+            self.variant = '300/400/500'
+            self.cruising_altitude = '11,300'
+        elif self.length in (121, 138):
+            self.variant = '600/700/800'
+            self.cruising_altitude = '12,500'
+
+    def drive_type(self):
+        print('Flight\n')
+
+    def autopilot(self):
+        print('Autopilot 2nd gen')
+
+    @property
+    def full_machine_info(self):
+        return self.info(), self.autopilot()
+
+
+class Concorde(Plane):
+
+    def __init__(self):
+        # self.cruising_altitude = '18,300'
+        super().__init__(model='Concorde', engine_type='Mk 610 turbojets with reheat,', max_speed='2,179',
+                         cruising_altitude='18,300')
+
+    def drive_type(self):
+        print('Supersonic flight')
+
+    @property
+    def full_machine_info(self):
+        return self.info()
 
 
 new_tesla = Tesla(battery_pover=90)
 new_land_rover = LandRover(engine_pover=500)
+new_boeing = Boeing(length=120)
+new_concorde = Concorde()
 
 
-for cars in (new_tesla, new_land_rover, ):
-    cars.full_car_info
-    cars.drive_type()
-
+for vehicles in (new_tesla, new_land_rover, new_boeing, new_concorde):
+    vehicles.full_machine_info
+    vehicles.drive_type()
